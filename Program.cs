257adaf024322432368d18d9d9db2640c1a4e1f5
Dominik Pflegerl <github.me@neus.xyz>
2020-12-11 19:31:08 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace SubtitlesApp {
+    class App {
+        private static void Main() {
+            Console.Title = "SubtitleApp";
+
+            //Console.Write("Season Folder: ");
+            // string pathToSeasonFolder = Console.ReadLine();
+            string pathSeasonFolder = @"C:\Mr Robot - S02";
+
+            string[] allEpisodeFolders = Directory.GetDirectories(pathSeasonFolder);
+
+            foreach(string episodeFolder in allEpisodeFolders) {
+                if(Subtitles.CheckForSubFolder(episodeFolder)) {
+                    Subtitles.MoveFiles(episodeFolder);
+
+                } else { 
+                }
+                DeleteNFO(episodeFolder);
+                Console.WriteLine();
+            }
+        }
+
+        public static void WriteLine(string text, ConsoleColor ccolor) {
+            //string colorFull = "ConsoleColor." + color;
+            //ConsoleColor ccolor = colorFull;
+
+            Console.BackgroundColor = ccolor;
+            Console.WriteLine(text);
+            Console.ResetColor();
+        }
+
+        public static void DeleteNFO(string episodeFolder) {
+            try {
+                foreach(string nfo in Directory.EnumerateFiles(episodeFolder, "*.nfo")) {
+                    File.Delete(nfo);
+                    WriteLine(episodeFolder + "  | .NFO deleted", ConsoleColor.DarkGreen);
+                }
+            } catch(Exception e) { Console.WriteLine(e.ToString()); };
+        }
+    }
+    class Subtitles {
+        public static bool CheckForSubFolder(string episodeFolder) {
+            if(Directory.Exists(episodeFolder + @"\Subs")) {
+                App.WriteLine(episodeFolder + "  | Subtitle folder found!", ConsoleColor.DarkGreen);
+                return true;
+            } else {
+                App.WriteLine(episodeFolder + "  | Error: Subtitle folder NOT found", ConsoleColor.DarkRed);
+                return false;
+            }
+        }
+
+        public static void MoveFiles(string episodeFolder) {
+            string subFolder = episodeFolder + @"\Subs";
+
+            try {
+                IEnumerable<FileInfo> files = Directory.GetFiles(subFolder).Select(f => new FileInfo(f));
+                foreach(var file in files) {
+                    File.Move(file.FullName, Path.Combine(episodeFolder, file.Name));
+                    App.WriteLine("| moving " + file.ToString(), ConsoleColor.DarkGreen);
+                }
+                
+                if(IsDirectoryEmpty(subFolder)) { 
+                    Directory.Delete(subFolder);
+                    App.WriteLine("| deleting " + subFolder.ToString(), ConsoleColor.DarkGreen);
+                };
+            }
+            catch(Exception e) { Console.WriteLine(e.ToString()); }
+
+        }
+
+        public static bool IsDirectoryEmpty(string path) {
+            return !Directory.EnumerateFileSystemEntries(path).Any();
+        }
+    }
+}
