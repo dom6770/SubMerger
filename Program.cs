39cb951f06c59359f39d5c4ef34b83a12334ad29
Dominik Pflegerl <github.me@neus.xyz>
2020-12-14 03:44:16 +0100

    smh smh the rest

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using 
+using System.Threading.Tasks;
+using Xabe.FFmpeg;
 
 namespace SubtitlesApp {
     class App {
@@ -11,16 +13,46 @@ namespace SubtitlesApp {
 
             //Console.Write("Season Folder: ");
             //string seasonFolder = Console.ReadLine();
-            string seasonFolder = @"C:\Mr Robot - S02";
+            string seasonFolder = @"E:\DOWNLOAD\Mr.Robot.S02.Complete.German.DL.1080p.BluRay.x264-RSG - Copy";
 
             string[] episodeFolderList = Directory.GetDirectories(seasonFolder);
 
+            Write("---", ConsoleColor.DarkRed);
+            Write("STARTING", ConsoleColor.Black);
+            WriteLine("---", ConsoleColor.DarkRed);
+            WriteLine(" ", ConsoleColor.Black);
+            WriteLine("We found " + episodeFolderList.Length + " episodes in the season folder", ConsoleColor.DarkBlue);
+            WriteLine(" ", ConsoleColor.Black);
+            WriteLine(" ", ConsoleColor.Black);
+
+            int count = 1;
+
             foreach(string episodeFolder in episodeFolderList) {
-                if(Folder.CheckForSubFolder(episodeFolder)) {
-                    Folder.MoveFiles(episodeFolder);
-                    MKV.Import(episodeFolder);
-                } else {}
+                Write(" #" + count++.ToString(), ConsoleColor.DarkGreen);
+                WriteLine(" " + episodeFolder, ConsoleColor.DarkGreen);
+                WriteLine(" ", ConsoleColor.Black);
+                WriteLine("      File count: " + Directory.GetFiles(episodeFolder).Length + " ", ConsoleColor.DarkGray);
+                WriteLine(" ", ConsoleColor.Black);
+
+                string[] allFiles = Directory.GetFiles(episodeFolder);
+
+                foreach(string file in allFiles) {
+                    WriteLine("      " + file, ConsoleColor.Black);
+                }
+
+                WriteLine(" ", ConsoleColor.Black);
+
                 Folder.DeleteNFO(episodeFolder);
+                if(Folder.CheckForSubFolder(episodeFolder)) { 
+                    WriteLine("      We found a subtitle folder, proceeding moving files and deleting folder...", ConsoleColor.Black); 
+                    Folder.MoveFiles(episodeFolder); 
+                }
+                if(Directory.GetFiles(episodeFolder).Length > 1) { 
+                    WriteLine("      Folder containts more than two files, proceeding importing subtitles into mkv...", ConsoleColor.Black); 
+                    MKV.Import(episodeFolder); 
+                } else {
+                    WriteLine("      We couldn't find any subtitles, proceeding with next episode...", ConsoleColor.Black);
+                }
                 Console.WriteLine();
             }
         }
@@ -38,10 +70,8 @@ namespace SubtitlesApp {
     class Folder {
         public static bool CheckForSubFolder(string episodeFolder) {
             if(Directory.Exists(episodeFolder + @"\Subs")) {
-                App.WriteLine(episodeFolder + "  | Subtitle folder found!", ConsoleColor.DarkGreen);
                 return true;
             } else {
-                App.WriteLine(episodeFolder + "  | Error: Subtitle folder NOT found", ConsoleColor.DarkRed);
                 return false;
             }
         }
@@ -50,15 +80,18 @@ namespace SubtitlesApp {
 
             try {
                 IEnumerable<FileInfo> files = Directory.GetFiles(subFolder).Select(f => new FileInfo(f));
+
+                App.WriteLine("   ", ConsoleColor.Black);
                 foreach(var file in files) {
                     File.Move(file.FullName, Path.Combine(episodeFolder, file.Name));
-                    App.WriteLine("| moving " + file.ToString(), ConsoleColor.DarkGreen);
+                    App.WriteLine("      File " + file.ToString() + " moved", ConsoleColor.Black);
                 }
                 
                 if(IsDirectoryEmpty(subFolder)) { 
                     Directory.Delete(subFolder);
-                    App.WriteLine("| deleting " + subFolder.ToString(), ConsoleColor.DarkGreen);
+                    App.WriteLine("      Folder " + subFolder.ToString() + " deleted", ConsoleColor.Black);
                 };
+                App.WriteLine("   ", ConsoleColor.Black);
             }
             catch(Exception e) { Console.WriteLine(e.ToString()); }
 
@@ -79,33 +112,54 @@ namespace SubtitlesApp {
     class MKV {
         public static void Import(string episodeFolder) {
             try {
-                string[] fileList = Directory.GetFiles(episodeFolder);
+                string[] allFiles = Directory.GetFiles(episodeFolder);
+                List<string> subFiles = new List<string>();
+                List<string> subFilesDel = new List<string>();
 
+                string mkvInputPath = "";
 
-                
-                //foreach(string file in fileList) {
-                //    App.WriteLine(file, ConsoleColor.Cyan);
-                //
-                //    //string endsWithidxEnglishFull =     "eng.idx";
-                //    //string endsWithsubEnglishFull =     "eng.sub";
-                //    //string endsWithidxEnglishForced =   "eng-forced.idx";
-                //    //string endsWithsubEnglishForced =   "eng-forced.sub";
-                //    //string endsWithidxGermanFull =      ".idx";
-                //    //string endsWithsubGermanFull =      ".sub";
-                //    //string endsWithidxGermanForced =    "-forced.dix";
-                //    string endsWithsubGermanForced =    "-forced.sub";
-                //
-                //    switch(file) {
-                //        case germanForced gerForced when file.EndsWith(endsWithsubGermanForced):
-                //            App.Write("hooray", ConsoleColor.Magenta);
-                //    }
-                //
-                //    if(file.EndsWith(endsWithsubGermanForced)) {
-                //        App.WriteLine("IT WORKED", ConsoleColor.Magenta);
-                //    }
-                //}
-            } catch(Exception e) {
-                App.WriteLine(e.ToString(), ConsoleColor.DarkRed);
+                foreach(string file in allFiles) {
+                    if(file.EndsWith("1080p.mkv")) {
+                        mkvInputPath = file;
+                    } else {
+                        subFiles.Add("'" + file + "' ");
+                        subFilesDel.Add(file);
+                    }
+                }; subFiles.Sort();
+
+                string mkvOutputPath = mkvInputPath.Remove(mkvInputPath.Length - 4, 4) ; mkvOutputPath += "_new.mkv";
+                if(File.Exists(mkvOutputPath)) { File.Delete(mkvOutputPath); }
+
+                string command =
+                    "mkvmerge -o '" + mkvOutputPath + "' '" + mkvInputPath + "' " +
+                    "--track-name 0:Full " + subFiles[0] + subFiles[1] +
+                    "--track-name 0:Full " + subFiles[2] + subFiles[3];
+                if(subFiles.Count > 4 && subFiles.Count < 6) {
+                    command += "--track-name 0:Forced " + subFiles[4] + subFiles[5];
+                }
+                if(subFiles.Count > 6) {
+                    command += "--track-name 0:Forced " + subFiles[6] + subFiles[7];
+                }
+
+                RunCommand(command);
+                RenameFile(mkvOutputPath, mkvInputPath);
+                foreach(string subFile in subFilesDel) {
+                    File.Delete(subFile);
+                }
+
+                App.WriteLine("  DONE  ", ConsoleColor.DarkGreen);
+            } catch(Exception e) { App.WriteLine(e.ToString(), ConsoleColor.DarkRed); }
+        }
+        public static void RunCommand(string command) {
+            ProcessStartInfo cmdsi = new ProcessStartInfo("powershell.exe");
+            cmdsi.Arguments = command;
+            Process cmd = Process.Start(cmdsi);
+            cmd.WaitForExit();
+        }
+        public static void RenameFile(string from, string to) {
+            if(File.Exists(from) && File.Exists(to)) {
+                File.Delete(to);
+                File.Move(from, to);
             }
         }
     }
