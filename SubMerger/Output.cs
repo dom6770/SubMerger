e68883525fdad0dc4b911855801d498902c9ae9d
Dominik Pflegerl <github.me@neus.xyz>
2022-08-31 11:36:24 +0200

    Removed logging to file as SABnzbd saves the console output anyway

--- SubMerger/Output.cs
+++ SubMerger/Output.cs
@@ -4,15 +4,7 @@ using System;
 using System.Linq;
 
 class Output {
-    public static void Write(StreamWriter log,string output) {
-        Console.Write(output);
-        log.Write(output);
-    }
-    public static void WriteLine(StreamWriter log,string output) {
-        Console.WriteLine(output);
-        log.WriteLine(output);
-    }
-    public static void WriteSeasonInfo(StreamWriter log,string inputPath) {
+    public static void WriteSeasonInfo(string inputPath) {
         string[] dirArray = Directory.GetDirectories(inputPath);
         int amountSubTotal = Folder.CountSubtitlesTotal(inputPath);
         int amountSubFullEng = Folder.CountFullEngSubfiles(inputPath);
@@ -20,49 +12,49 @@ class Output {
         int amountSubMissing = dirArray.Length - amountSubTotal;
 
         // Header - Type
-        Output.WriteLine(log,"Season");
+        Console.WriteLine("Season");
 
         // Header - Episodes Amount
         int episodeCount = 0;
         foreach(string dir in Directory.GetDirectories(inputPath))
             if(Regex.Match(dir,@"S[0-9]{2}E[0-9]{2}").Success)
                 episodeCount++;
-        Write(log,"|- Episodes: " + episodeCount + "\n");
+        Console.Write("|- Episodes: " + episodeCount + "\n");
 
         // Header - Subtitles
-        WriteLine(log,"|- Subtitles");
+        Console.WriteLine("|- Subtitles");
         // Header - Subtitles Total Found
-        Write(log,"|  - Full     " + amountSubFullEng.ToString("00") + "  ( ");
+        Console.Write("|  - Full     " + amountSubFullEng.ToString("00") + "  ( ");
         foreach(string dir in dirArray)
             if(Directory.Exists(dir + @"\Subs") && Directory.GetFiles(dir + @"\Subs\","*eng.*").Any())
-                Write(log,Regex.Match(dir,@"E[0-9]{2}").Groups[0].Value + " ");
-        Write(log,")\n");
+                Console.Write(Regex.Match(dir,@"E[0-9]{2}").Groups[0].Value + " ");
+        Console.Write(")\n");
 
         // Header - Subtitles Missing Eng Found
-        Write(log,"|  - !Full    " + amountSubMissingEng.ToString("00") + "  ( ");
+        Console.Write("|  - !Full    " + amountSubMissingEng.ToString("00") + "  ( ");
         foreach(string dir in dirArray)
             if(Directory.Exists(dir + @"\Subs") && !Directory.GetFiles(dir + @"\Subs\","*eng.*").Any())
-                Write(log,Regex.Match(dir,@"E[0-9]{2}").Groups[0].Value + " ");
-        Write(log,")\n");
+                Console.Write(Regex.Match(dir,@"E[0-9]{2}").Groups[0].Value + " ");
+        Console.Write(")\n");
 
         // Header - Subtitles Missing Eng Found
-        Write(log,"|  - Missing  " + amountSubMissing.ToString("00") + "  ( ");
+        Console.Write("|  - Missing  " + amountSubMissing.ToString("00") + "  ( ");
         foreach(string dir in dirArray)
             if(!Directory.Exists(dir + @"\Subs"))
-                Write(log,Regex.Match(dir,@"E[0-9]{2}").Groups[0].Value + " ");
-        WriteLine(log,")\n");
+                Console.Write(Regex.Match(dir,@"E[0-9]{2}").Groups[0].Value + " ");
+        Console.WriteLine(")\n");
     }
-    public static void WriteInfo(StreamWriter log,string inputPath) {
-        WriteLine(log,"Single File");
+    public static void WriteInfo(string inputPath) {
+        Console.WriteLine("Single File");
         // Header - Subtitles
-        Write(log,"|- Subtitles");
+        Console.Write("|- Subtitles");
 
         if(Directory.Exists(inputPath + @"\Subs")) {
-            Write(log,"\n");
+            Console.Write("\n");
             if(Directory.GetFiles(inputPath + @"\Subs","*.sub").Length > 0)
                 foreach(string file in Directory.GetFiles(inputPath + @"\Subs","*.sub"))
-                    WriteLine(log,"|  - " + file.Remove(0,inputPath.Length + 6));
-        } else Write(log," missing");
-        WriteLine(log,"");
+                    Console.WriteLine("|  - " + file.Remove(0,inputPath.Length + 6));
+        } else Console.Write(" missing");
+        Console.WriteLine("");
     }
 }
--- SubMerger/Program.cs
+++ SubMerger/Program.cs
@@ -2,6 +2,9 @@
 class Program {
     private static int Main(string[] args) {
         SubMerger subMerger = new();
+        subMerger.inputName = args[0];
+        subMerger.inputPath = args.Length > 1 ? args[1] : DateTime.Now.ToString("dd-MM_HH-mm");
+
         return subMerger.Run(args);
     }
 }
