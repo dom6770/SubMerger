421360d1cfcf2b31556934a7ffc4696ed2199f18
dom6770 <github.me@neus.xyz>
2024-08-15 09:27:23 +0200

    feeat: Added error handling for missing arguments

--- SubMerger/Program.cs
+++ SubMerger/Program.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 class Program {
     private static int Main(string[] args) {
-        
+
         //subMerger.inputName = args[0];
         //subMerger.inputPath = args.Length > 1 ? args[1] : DateTime.Now.ToString("dd-MM_HH-mm");
+
+        if(args.Length == 0) {
+            Console.WriteLine("Error: No arguments provided. Please specify the input path as first, and optionally the movie name as second argument.");
+            return 1; // Non-zero exit code indicates an error
+        }
 
         SubMerger subMerger = new(
             args[0],
