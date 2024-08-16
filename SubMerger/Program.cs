e0abb93fc34243a6cbfac1fa0807bbd321bb0432
dom6770 <github.me@neus.xyz>
2024-08-16 13:21:09 +0200

    feat: SubMerger now directly calls the mkvmerge without any shell (bash/pwsh).

--- SubMerger/Program.cs
+++ SubMerger/Program.cs
@@ -9,22 +9,18 @@ class Program {
 
         if(args.Length == 0) {
             Console.WriteLine("Error: No arguments provided. Please specify the input path as first, and optionally the movie name as second argument.");
-            return 1; // Non-zero exit code indicates an error
+            return 1;         
         }
 
         if(!IsCommandAvailable("mkvmerge")) {
-            Console.WriteLine("Error: mkvmerge not found in PATH.");
-            Environment.Exit(1);
-        }
-        if(!IsCommandAvailable("pwsh")) {
-            Console.WriteLine("Error: pwsh not found in PATH.");
-            Environment.Exit(1);
-        }
+            Console.WriteLine("Error: mkvmerge not found in PATH. Please install mkvtoolnix");
+            return 2;
+        } 
 
         SubMerger subMerger = new(
             args[0],
             args.Length > 1 ? args[1] : DateTime.Now.ToString("dd-MM_HH-mm")
-            );
+        );
 
         return subMerger.Run();
     }
--- SubMerger/mkvmerge.cs
+++ SubMerger/mkvmerge.cs
@@ -1,6 +1,6 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.IO;
-using System;
 using System.Linq;
 
 class mkvmerge {
@@ -8,7 +8,8 @@ class mkvmerge {
         try {
             string mkvInputPath = Directory.GetFiles(episodeFolder, "*.mkv").First();
             string mkvInputName = Path.GetFileNameWithoutExtension(mkvInputPath);
-            string mkvOutputPath = mkvInputPath.Remove(mkvInputPath.Length - 4, 4); mkvOutputPath += "_new.mkv";
+            string mkvOutputPath = mkvInputPath.Remove(mkvInputPath.Length - 4, 4) + "_new.mkv";
+
             string[] subtitlesIdx = Directory.GetFiles(episodeFolder, "*.idx");
             string[] subtitlesSub = Directory.GetFiles(episodeFolder, "*.sub");
 
@@ -17,35 +18,47 @@ class mkvmerge {
             string subtitleGerFull = mkvInputName;
             string subtitleGerForced = mkvInputName + "*forced";
 
-            string mkvmergeArgs = "-o '" + mkvOutputPath + "' -q --no-subtitles '" + mkvInputPath + "' ";
-            if(Folder.SubfilesExist(episodeFolder,subtitleEngFull)) {
-                mkvmergeArgs += "--language 0:eng " +
-                                "--track-name 0:Full '"
-                                + Directory.GetFiles(episodeFolder,subtitleEngFull + ".idx")[0] + "' '"
-                                + Directory.GetFiles(episodeFolder,subtitleEngFull + ".sub")[0] + "' ";
+            var mkvmergeArgs = new[]{
+                "--output", mkvOutputPath, "--quiet", "--no-subtitles", mkvInputPath
+            }.ToList();
+
+            if(Folder.SubfilesExist(episodeFolder, subtitleEngFull)) {
+                mkvmergeArgs.AddRange(new[] {
+                    "--language", "0:eng", "--track-name", "0:Full",
+                    Directory.GetFiles(episodeFolder, subtitleEngFull + ".idx")[0],
+                    Directory.GetFiles(episodeFolder, subtitleEngFull + ".sub")[0]
+                });
             }
-            if(Folder.SubfilesExist(episodeFolder,subtitleEngForced)) {
-                mkvmergeArgs += "--language 0:eng " +
-                                "--track-name 0:Forced '"
-                                + Directory.GetFiles(episodeFolder,subtitleEngForced + ".idx")[0] + "' '"
-                                + Directory.GetFiles(episodeFolder,subtitleEngForced + ".sub")[0] + "' ";
+
+            if(Folder.SubfilesExist(episodeFolder, subtitleEngForced)) {
+                mkvmergeArgs.AddRange(new[] {
+                    "--language", "0:eng", "--track-name", "0:Forced",
+                    Directory.GetFiles(episodeFolder, subtitleEngForced + ".idx")[0],
+                    Directory.GetFiles(episodeFolder, subtitleEngForced + ".sub")[0]
+                });
             }
-            if(Folder.SubfilesExist(episodeFolder,subtitleGerFull)) {
-                mkvmergeArgs += "--language 0:deu " +
-                                "--track-name 0:Full '"
-                                + Directory.GetFiles(episodeFolder,subtitleGerFull + ".idx")[0] + "' '"
-                                + Directory.GetFiles(episodeFolder,subtitleGerFull + ".sub")[0] + "' ";
+
+            if(Folder.SubfilesExist(episodeFolder, subtitleGerFull)) {
+                mkvmergeArgs.AddRange(new[] {
+                    "--language", "0:deu", "--track-name", "0:Full",
+                    Directory.GetFiles(episodeFolder, subtitleGerFull + ".idx")[0],
+                    Directory.GetFiles(episodeFolder, subtitleGerFull + ".sub")[0]
+                });
             }
-            if(Folder.SubfilesExist(episodeFolder,subtitleGerForced)) {
-                mkvmergeArgs += "--language 0:deu " +
-                                "--track-name 0:Forced '"
-                                + Directory.GetFiles(episodeFolder,subtitleGerForced + ".idx")[0] + "' '"
-                                + Directory.GetFiles(episodeFolder,subtitleGerForced + ".sub")[0] + "' ";
+
+            if(Folder.SubfilesExist(episodeFolder, subtitleGerForced)) {
+                mkvmergeArgs.AddRange(new[] {
+                    "--language", "0:deu", "--track-name", "0:Forced",
+                    Directory.GetFiles(episodeFolder, subtitleGerForced + ".idx")[0],
+                    Directory.GetFiles(episodeFolder, subtitleGerForced + ".sub")[0]
+                });
             }
 
             if(File.Exists(mkvOutputPath)) { File.Delete(mkvOutputPath); }
-            Run(mkvmergeArgs);
-            Folder.RenameFile(mkvOutputPath,mkvInputPath);
+
+            Run(mkvmergeArgs.ToArray());
+
+            Folder.RenameFile(mkvOutputPath, mkvInputPath);
             Folder.DeleteSubtitleFiles(subtitlesIdx);
             Folder.DeleteSubtitleFiles(subtitlesSub);
         }
@@ -53,12 +66,26 @@ class mkvmerge {
             Console.WriteLine(e.ToString());
         }
     }
-    public static void Run(string mkvmergeArgs) {
-        ProcessStartInfo command = new("pwsh") {
-            Arguments = $"-command \"& mkvmerge {mkvmergeArgs}\""
+
+    public static void Run(string[] mkvmergeArgs)
+    {
+        ProcessStartInfo command = new()
+        {
+            FileName = "mkvmerge",          // Directly call mkvmerge
+            Arguments = string.Join(" ", mkvmergeArgs), // Join arguments with spaces
+            RedirectStandardOutput = true,  // Capture the output
+            RedirectStandardError = true,   // Capture any errors
+            UseShellExecute = false,        // Don't use a shell, run the command directly
+            CreateNoWindow = true           // Run without creating a window
         };
-        // Console.WriteLine("ProcessStartInfo: " + command.Arguments);
-        Process pwsh = Process.Start(command);
-        pwsh.WaitForExit();
+
+        using Process process = Process.Start(command);
+        string error = process.StandardError.ReadToEnd();
+        process.WaitForExit();
+
+        if (!string.IsNullOrEmpty(error)) {
+            Console.WriteLine($"Error: {error}");
+        }
+        
     }
 }
